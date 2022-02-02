225821e8550860054fcfb2a45cb155687d6697d6
kateryna klimchuk <ketyclima@gmail.com>
2022-02-02 14:46:08 +0200

    add main files and 2 tasks

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-autocheck
+
+Save here my solutions of js-autocheck tasks
---
+++ index.js
@@ -0,0 +1,67 @@
+// M-2. Arrays and function
+
+
+// =========== Searching common elements at two arrays ================
+
+
+const element = function getCommonElements(array1, array2) {
+  // Change code below this line
+    let commonArray = [];
+
+    for (const element of array1) {
+        for (const element2 of array2) {
+            if (array2.includes(element)) {
+                commonArray.push(element);
+                break;
+            }
+            
+        }
+    }
+    
+
+    return commonArray;
+
+ // Change code above this line
+}
+
+const total = element([2, 2, 3, 5, 6], [4, 5, 6, 10]);
+
+console.log(total);
+
+
+// M-3. Objects
+
+// ============ Adding object to object ===============
+
+// Добавь объекту apartment несколько новых свойств:
+
+// area - площадь в квадратных метрах, число 60;
+// rooms - количество комнат, число 3;
+// location - местоположение квартиры, обьект со следующими вложенными свойствами;
+// country - страна, строка "Jamaica";
+// city - город, строка "Kingston".
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {
+    country: 'Jamaica',
+    city: 'Kingston',
+};
+
+
+console.log(apartment.location.city);
+
